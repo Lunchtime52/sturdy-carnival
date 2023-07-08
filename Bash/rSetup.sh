714ca7760dbefc16affe41f0e98719368d02d15f
Ryan <ryan@swtichitoffice.com>
2023-07-07 23:28:47 -0400

    iLate night coding

--- Bash/rSetup.sh
+++ Bash/rSetup.sh
@@ -18,52 +18,54 @@ apps=(
 
 DownloadApp ()
 {
-  if command -v "$1" >/dev/null 2>&1; then
+  if [ -z "$(which $1)" ]; then
+
+    if [[$1 == "rg" ]]; then
+      $1="ripgrep"
+    fi
+
+    if snap find $1 > /dev/null; then
+      sudo snap install $1 --classic --edge
+    else 
+      sudo apt-get install $1 > /dev/null
+    fi
+
+  else
     echo "Already installed $1 skipping"
-    return
   fi
 
-  if snap find "$1" &>/dev/null; then
-    sudo snap install $1 --classic --edge
-    return
-  fi
-
-  if apt-cache polcy "$1" >/dev/null; then
-    sudo apt-get install $1 >/dev/null
-    return
-  fi
 }
 Chekcfile ()
 {
-  if [[ ! -f "~/$1" ]]; then
-    echo "Creating File ~/$1"
-    if [[ $1 == ".bash_aliases" ]]; then
+  if [[ ! -f $1 ]]; then
+    echo "Creating File $1"
+    if [[ $1 == "~/.bash_aliases" ]]; then
       cp ~/sturdy-carnival/Bash/.bash_aliases ~/.bash_aliases
     else
-      touch ~/$1
+      touch $1
     fi    
-    else
-      echo "The file $HOME/$1 already exists."
+  else
+    echo "The file $1 already exists."
   fi
 }
 Git ()
 {
-   echo "Please enter your GitHub Username."
-   read username
-   echo "Please enter your email to use with git."
-   read email
-   echo "Please enter your personal key."
-   read key
-   git config --global user.name $username
-   git config --global user.email $email
-   git config --global credential.username $username
-   git config --global credential.password $key
-   git config --global credential.helper store
-   git config --global core.editor nvim
+  echo "Please enter your GitHub Username."
+  read username
+  echo "Please enter your email to use with git."
+  read email
+  echo "Please enter your personal key."
+  read key
+  git config --global user.name $username
+  git config --global user.email $email
+  git config --global credential.username $username
+  git config --global credential.password $key
+  git config --global credential.helper store
+  git config --global core.editor nvim
 }
 
-Chekcfile .hushlogin
-Chekcfile .bash_aliases
+Chekcfile ~/.hushlogin
+Chekcfile ~/.bash_aliases
 for app in ${apps[@]}
 do
   DownloadApp $app
@@ -71,7 +73,9 @@ done
 sudo snap refresh && sudo apt update && sudo apt upgrade
 #git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 #nvim ~/.config/nvim/lua/custom/
-Git
+if [[ ! -f ~/.gitconfig ]]; then
+  Git
+fi
 
 
 
